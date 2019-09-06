bd08bfebddf8999d59a09f53177d3d03a5fb4b20
priskilatandi <priskilatandi2598@gmail.com>
2019-09-06 17:27:23 +0700

    week04

---
+++ Pertemuan 4/umn-homes/src/app/places/offers/offers.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Place } from '../place.model';
+import { PlacesService } from '../places.service';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-offers',
+  templateUrl: './offers.page.html',
+  styleUrls: ['./offers.page.scss'],
+})
+export class OffersPage implements OnInit {
+
+  loadedPlaces: Place[];
+
+  constructor(private placesService: PlacesService, private menuCtrl: MenuController) {}
+
+  ngOnInit() {
+    this.loadedPlaces = this.placesService.places;
+  }
+
+  onOpenMenu(){
+    this.menuCtrl.toggle('m1');
+  }
+
+}
